53329a2ecb0989cd85bc0c565755cc0dda01f3c4
Vinyy322 <viniciusdev462@outlook.com>
2024-06-04 17:23:34 +0000

    Versao_2

--- main.cpp
+++ main.cpp
@@ -1,39 +1,49 @@
-const int led_1 = 15;
-const int led_2 = 2;
-const int led_3 = 0;
+//variaveis da porta do led
+const int led_v1 = 15;
+const int led_a1 = 2;
+const int led_vm1 = 0;
 
-const int led_4 = 5;
-const int led_5 = 18;
-const int led_6 = 19;
+const int led_v2 = 5;
+const int led_a2 = 18;
+const int led_vm2 = 19;
+
+float temp = 2;
+
 
 void setup() {
-  pinMode(led_1, OUTPUT);
-  pinMode(led_2, OUTPUT);
-  pinMode(led_3, OUTPUT);
-  pinMode(led_4, OUTPUT);
-  pinMode(led_5, OUTPUT);
-  pinMode(led_6, OUTPUT);
+
+  pinMode(led_v1, OUTPUT);//semaforo 1
+  pinMode(led_a1, OUTPUT);//semaforo 1
+  pinMode(led_vm1, OUTPUT);//semaforo 1
+
+  pinMode(led_v2, OUTPUT);//semaforo 2
+  pinMode(led_a2, OUTPUT);//semaforo 2
+  pinMode(led_vm2, OUTPUT);//semaforo 2
   Serial.begin(115200);
 }
 
 void loop() {
-  apagarLed(led_3);
-  acenderLed(led_1);
-  acenderLed(led_6);
-  tempo(2);
-  apagarLed(led_1);
-  acenderLed(led_2);
-  tempo(2);
-  apagarLed(led_6);
-  apagarLed(led_2);
-  acenderLed(led_3);
-  acenderLed(led_4);
-  tempo(2);
-  apagarLed(led_4);
-  acenderLed(led_5);
-  tempo(2);
-  apagarLed(led_5);
-  apagarLed(led_4);
+  Ligado();
+}
+
+void Ligado(){
+  apagarLed(led_vm1);//semaforo 1(apagar vermelho)
+  acenderLed(led_v1);//semaforo 1
+  acenderLed(led_vm2);//semaforo 2(acender vermelho)
+  tempo(temp);
+  apagarLed(led_v1);//semaforo 1
+  acenderLed(led_a1);//semaforo 1
+  tempo(temp);
+  apagarLed(led_vm2);//semaforo 2
+  apagarLed(led_a1);//semaforo 1
+  acenderLed(led_vm1);//semaforo 1
+  acenderLed(led_v2);//semaforo 2
+  tempo(temp);
+  apagarLed(led_v2);//semaforo 2
+  acenderLed(led_a2);//semaforo 2
+  tempo(temp);
+  apagarLed(led_a2);//semaforo 2
+  apagarLed(led_v2);//semaforo 2
 }
 
 
